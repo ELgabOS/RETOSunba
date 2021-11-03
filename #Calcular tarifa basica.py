e6af38f3e27babddc3e050f5bae0cc46329bcc00
ELgabOS <gabrielcortesperez1@gmail.com>
2021-11-02 20:57:03 -0500

    Create #Calcular tarifa basica.py

---
+++ #Calcular tarifa basica.py
@@ -0,0 +1,26 @@
+#Calcular tarifa basica
+
+#entrada
+Nombre_usuario=input("nombre:  ")
+Estrato_usuario=int (input("estrato 1, 2,3 ,4 ,5:  ")
+
+#proceso
+
+if Estrato_usuario==1:
+    tarifa_basica=10000
+
+elif Estrato_usuario==2:
+    tarifa_basica=15000
+
+elif Estrato_usuario==3:
+    tarifa_basica=30000
+
+elif Estrato_usuario==4:
+    tarifa_basica=50000
+
+else:
+    tarifa_basica=650000
+
+#salida
+
+#falta validacion
